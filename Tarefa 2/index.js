00cdc0ae94b2ab8ca71d6b183cbdc3e7da45806e
taamyz <tamires.trindade15@gmail.com>
2023-06-28 19:26:27 -0300

    task 2

---
+++ Tarefa 2/index.js
@@ -0,0 +1,22 @@
+
+const question = prompt("Em que ano você nasceu? - Somente Números");
+
+
+if (question >= 1940 && question <= 1960 ) {
+    alert("Você é Baby Boomer");
+}
+else if (question >= 1961 && question <= 1980 ) {
+    alert("Você é Geração X");
+}
+else if (question >= 1981 && question <= 1994 ) {
+    alert("Você é Millenial");
+}
+else if (question >= 1995 && question <= 2010 ) {
+    alert("Você é Gen Z");
+}
+else if (question >= 2011 ) {
+    alert("Você é Alpha");
+}
+else {
+    alert("Digite apenas sua idade em números, exemplo: 2001")
+};
