0667d1df02d62cd9d3b646967087ac271cfa36af
taamyz <tamires.trindade15@gmail.com>
2023-06-28 19:58:25 -0300

    task 2

---
+++ task1/index.js
@@ -0,0 +1,7 @@
+const quote1 = prompt("Escreva uma frase");
+const quote2 = prompt("Agora, escreva algo para combinar com ela:");
+
+const result = quote1 + " " + quote2;
+
+
+alert("Sua frase final é:" + result);
